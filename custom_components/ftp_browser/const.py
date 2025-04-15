8b39e4467f5c8b79347b485cd5b32b618dfb6820
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-15 04:16:56 +0200

    Update const.py

--- custom_components/ftp_browser/const.py
+++ custom_components/ftp_browser/const.py
@@ -10,12 +10,14 @@ CONF_PORT = "port"
 CONF_SSL = "ssl"
 CONF_SCAN_INTERVAL = "scan_interval"
 CONF_SHARE_DURATION = "share_duration"
+CONF_ROOT_PATH = "root_path"
 
 # Defaults
 DEFAULT_PORT = 21
 DEFAULT_SSL = False
 DEFAULT_SCAN_INTERVAL = 300  # 5 minutes
 DEFAULT_SHARE_DURATION = 24  # 24 hours
+DEFAULT_ROOT_PATH = "/sdcard"  # Chemin racine par défaut
 
 # Services
 SERVICE_CREATE_SHARE = "create_share"
@@ -24,3 +26,4 @@ SERVICE_DELETE_SHARE = "delete_share"
 # Storage
 STORAGE_KEY = "ftp_browser.shared_links"
 STORAGE_VERSION = 1
+
