4b74d7e18653efefab6ac9f74464526e2f6ed1e7
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-15 03:34:26 +0200

    Create const.py

---
+++ custom_components/ftp_browser/const.py
@@ -0,0 +1,26 @@
+"""Constants for the FTP Browser integration."""
+
+DOMAIN = "ftp_browser"
+
+# Configuration
+CONF_FTP_SERVER = "ftp_server"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_PORT = "port"
+CONF_SSL = "ssl"
+CONF_SCAN_INTERVAL = "scan_interval"
+CONF_SHARE_DURATION = "share_duration"
+
+# Defaults
+DEFAULT_PORT = 21
+DEFAULT_SSL = False
+DEFAULT_SCAN_INTERVAL = 300  # 5 minutes
+DEFAULT_SHARE_DURATION = 24  # 24 hours
+
+# Services
+SERVICE_CREATE_SHARE = "create_share"
+SERVICE_DELETE_SHARE = "delete_share"
+
+# Storage
+STORAGE_KEY = "ftp_browser.shared_links"
+STORAGE_VERSION = 1
